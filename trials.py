6324d513369163eea9664171ecc2550abe24ed62
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2021-04-21 03:52:30 +0000

    Added python function to print as numbered list

--- trials.py
+++ trials.py
@@ -25,7 +25,10 @@ def get_odd_indices(items):
 
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    i = 1
+    for item in items:
+        print(f'{i}. {item}')
+        i += 1
 
 
 def get_range(start, stop):
